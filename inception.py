21835e5a5a6364e660c2c4e1d31fa512a7be4823
Maximilian Berger <git-commit@users.noreply.github.com>
2018-11-17 18:42:48 +0100

    Make sure that tensor flow uses the gpu

--- inception.py
+++ inception.py
@@ -1,9 +1,16 @@
 import os
+import tensorflow as tf
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras import backend as K
+
+# make sure soft-placement is off
+tf_config = tf.ConfigProto(allow_soft_placement=False)
+tf_config.gpu_options.allow_growth = True
+s = tf.Session(config=tf_config)
+K.set_session(s)
 
 num_classes = 12
 size = (299, 299)
