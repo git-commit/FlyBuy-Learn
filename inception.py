109f4e9be60a5f93c56bd34f556412d7233c5744
Maximilian Berger <git-commit@users.noreply.github.com>
2018-11-17 18:43:16 +0100

    Also measure accuracy when fine tuning the model in the second step

--- inception.py
+++ inception.py
@@ -88,8 +88,8 @@ model.fit_generator(train_generator,
 
 # let's visualize layer names and layer indices to see how many layers
 # we should freeze:
-for i, layer in enumerate(base_model.layers):
-   print(i, layer.name)
+#for i, layer in enumerate(base_model.layers):
+#   print(i, layer.name)
 
 # we chose to train the top 2 inception blocks, i.e. we will freeze
 # the first 249 layers and unfreeze the rest:
@@ -98,10 +98,15 @@ for layer in model.layers[:249]:
 for layer in model.layers[249:]:
    layer.trainable = True
 
+print("Fine tuning...")
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
 from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+model.compile(
+    optimizer=SGD(lr=0.0001, momentum=0.9),
+    loss='categorical_crossentropy',
+    metrics=['acc']
+    )
 
 # we train our model again (this time fine-tuning the top 2 inception blocks
 # alongside the top Dense layers
